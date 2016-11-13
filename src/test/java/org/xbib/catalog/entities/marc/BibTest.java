9b4e674e0355c8c7d2ddb8a22ce17b565a7c8626
Jörg Prante <joergprante@gmail.com>
2016-11-13 20:32:06 +0100

    update to content-rf 1.0.4, setHandler() test

--- src/test/java/org/xbib/catalog/entities/marc/BibTest.java
+++ src/test/java/org/xbib/catalog/entities/marc/BibTest.java
@@ -1,12 +1,15 @@
 package org.xbib.catalog.entities.marc;
 
+import static org.junit.Assert.assertTrue;
 import static org.xbib.content.rdf.RdfXContentFactory.rdfXContentBuilder;
+import static org.xbib.content.rdf.RdfXContentFactory.routeRdfXContentBuilder;
 
 import org.junit.Test;
 import org.xbib.catalog.entities.CatalogEntityBuilder;
 import org.xbib.catalog.entities.CatalogEntityWorkerState;
 import org.xbib.content.rdf.RdfContentBuilder;
 import org.xbib.content.rdf.RdfXContentParams;
+import org.xbib.content.rdf.RouteRdfXContentParams;
 import org.xbib.content.resource.IRI;
 import org.xbib.marc.Marc;
 import org.xbib.marc.MarcXchangeConstants;
@@ -14,6 +17,7 @@ import org.xbib.marc.MarcXchangeConstants;
 import java.io.IOException;
 import java.net.URL;
 import java.text.MessageFormat;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -47,6 +51,21 @@ public class BibTest {
                     .build()
                     .xmlReader().parse();
             logger.log(Level.INFO, MessageFormat.format("unmapped ZDB OAI fields = {0}", myBuilder.getUnmapped()));
+            assertTrue(myBuilder.getCounter().get() > 0);
+        }
+    }
+
+
+    @Test
+    public void testBibToRoute() throws Exception {
+        try (MyRouteBuilder myBuilder = new MyRouteBuilder(getClass().getResource("bib.json"))) {
+            Marc.builder()
+                    .setInputStream(getClass().getResource("zdbtitutf8.mrc").openStream())
+                    .setMarcRecordListener(myBuilder)
+                    .build()
+                    .writeRecords();
+            logger.log(Level.INFO, MessageFormat.format("unmapped ZDB Bib fields = {0}", myBuilder.getUnmapped()));
+            assertTrue(myBuilder.getCount() > 0);
         }
     }
 
@@ -72,6 +91,40 @@ public class BibTest {
             } catch (IOException e) {
                 // ignore
             }
+        }
+    }
+
+    private class MyRouteBuilder extends CatalogEntityBuilder {
+
+        final AtomicInteger counter = new AtomicInteger();
+
+        MyRouteBuilder(URL url) throws Exception {
+            super("org.xbib.catalog.entities.marc.zdb.bib", url);
+        }
+
+        @Override
+        public void afterFinishState(CatalogEntityWorkerState state) {
+            try {
+                IRI iri = IRI.builder().scheme("http")
+                        .host("zdb")
+                        .query("title")
+                        .fragment(Long.toString(counter.getAndIncrement())).build();
+                state.getResource().setId(iri);
+                RouteRdfXContentParams params = new RouteRdfXContentParams();
+                // testing the setHandler() method
+                params.setHandler((content, i) -> {
+                    logger.log(Level.INFO, content);
+                    counter.incrementAndGet();
+                });
+                RdfContentBuilder<RouteRdfXContentParams> builder = routeRdfXContentBuilder(params);
+                builder.receive(state.getResource());
+            } catch (IOException e) {
+                // ignore
+            }
+        }
+
+        public int getCount() {
+            return counter.get();
         }
     }
 }
