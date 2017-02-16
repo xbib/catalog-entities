71ce6561ccfe4abd0ea404159953765e9456b7bc
Jörg Prante <joergprante@gmail.com>
2017-02-17 00:53:11 +0100

    clean up ZDB bib/hol gen info and phys desc, move to version 1.0.14

--- src/main/java/org/xbib/catalog/entities/marc/zdb/hol/PhysicalDescriptionCode.java
+++ src/main/java/org/xbib/catalog/entities/marc/zdb/hol/PhysicalDescriptionCode.java
@@ -2,12 +2,11 @@ package org.xbib.catalog.entities.marc.zdb.hol;
 
 import org.xbib.catalog.entities.CatalogEntity;
 import org.xbib.catalog.entities.CatalogEntityWorker;
+import org.xbib.content.rdf.Resource;
 import org.xbib.marc.MarcField;
 
 import java.io.IOException;
-import java.text.MessageFormat;
 import java.util.Map;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -17,46 +16,38 @@ public class PhysicalDescriptionCode extends CatalogEntity {
 
     private static final Logger logger = Logger.getLogger(PhysicalDescriptionCode.class.getName());
 
+    private final Map<String, Object> codes;
+
+    @SuppressWarnings("unchecked")
     public PhysicalDescriptionCode(Map<String, Object> params) {
         super(params);
+        this.codes = (Map<String, Object>) getParams().get("codes");
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public CatalogEntity transform(CatalogEntityWorker worker, MarcField field) throws IOException {
-        String value = getValue(field);
-        Map<String, Object> codes = (Map<String, Object>) getParams().get("codes");
-        if (codes == null) {
-            logger.log(Level.WARNING, () -> MessageFormat.format("no 'codes' for {0}", value));
-            return super.transform(worker, field);
-        }
-        // position 0 is the selector
-        codes = (Map<String, Object>) codes.get("0");
-        if (value != null) {
-            check(worker, codes, value);
-        }
-        for (MarcField.Subfield subfield : field.getSubfields()) {
-            check(worker, codes, subfield.getValue());
-        }
-        return super.transform(worker, field);
-    }
-
-    @SuppressWarnings("unchecked")
-    private void check(CatalogEntityWorker worker,
-                       Map<String, Object> codes, String data) throws IOException {
-        Map<String, Object> m = (Map<String, Object>) codes.get(data.substring(0, 1));
-        if (m == null) {
-            return;
-        }
-        // transform all codes except position 0
-        String predicate = (String) m.get("_predicate");
-        for (int i = 1; i < data.length(); i++) {
-            String ch = data.substring(i, i + 1);
-            Map<String, Object> q = (Map<String, Object>) m.get(Integer.toString(i));
-            if (q != null) {
-                String code = (String) q.get(ch);
-                worker.getWorkerState().getResource().add(predicate, code);
+        if (codes != null) {
+            // position 0 is the selector
+            Map<String, Object> subcodes = (Map<String, Object>) codes.get("0");
+            String value = getValue(field);
+            if (value != null) {
+                Map<String, Object> m = (Map<String, Object>) subcodes.get(value.substring(0, 1));
+                if (m != null) {
+                    Resource resource = worker.getWorkerState().getResource().newResource(getClass().getSimpleName());
+                    // transform all codes except position 0
+                    for (int i = 1; i < value.length(); i++) {
+                        String ch = value.substring(i, i + 1);
+                        Map<String, Object> q = (Map<String, Object>) m.get(Integer.toString(i));
+                        if (q != null) {
+                            String predicate = (String) q.get("_predicate");
+                            String code = (String) q.get(ch);
+                            resource.add(predicate, code);
+                        }
+                    }
+                }
             }
         }
+        return super.transform(worker, field);
     }
 }
