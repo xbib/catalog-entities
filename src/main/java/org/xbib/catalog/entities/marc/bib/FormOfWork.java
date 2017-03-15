cf8aba84f28d42fee818a8cd5873022b4f7eead3
Jörg Prante <joergprante@gmail.com>
2017-03-15 12:49:23 +0100

    add FormOfWork and NumericDesignationOfMusicalWork, add some ZDB relation patterns

---
+++ src/main/java/org/xbib/catalog/entities/marc/bib/FormOfWork.java
@@ -0,0 +1,15 @@
+package org.xbib.catalog.entities.marc.bib;
+
+import org.xbib.catalog.entities.CatalogEntity;
+
+import java.util.Map;
+
+/**
+ *
+ */
+public class FormOfWork extends CatalogEntity {
+
+    public FormOfWork(Map<String, Object> params) {
+        super(params);
+    }
+}
---
+++ src/main/java/org/xbib/catalog/entities/marc/bib/NumericDesignationOfMusicalWork.java
@@ -0,0 +1,15 @@
+package org.xbib.catalog.entities.marc.bib;
+
+import org.xbib.catalog.entities.CatalogEntity;
+
+import java.util.Map;
+
+/**
+ *
+ */
+public class NumericDesignationOfMusicalWork extends CatalogEntity {
+
+    public NumericDesignationOfMusicalWork(Map<String, Object> params) {
+        super(params);
+    }
+}
